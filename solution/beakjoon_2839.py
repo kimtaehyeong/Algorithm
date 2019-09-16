193960bf01fd4a61089bff597ab37b866d86cab9
taehyung.kim <kimtaehyeong62@gmail.com>
2019-09-16 23:43:04 +0900

    beakjoon_2839

---
+++ solution/beakjoon_2839.py
@@ -0,0 +1,12 @@
+inp = int(input())
+Box = 0
+while True
+    if (inp % 5) == 0
+        Box = Box + (inp5)
+        print(Box)
+        break
+    inp = inp-3
+    Box += 1
+    if inp  0
+        print(-1)
+        break
